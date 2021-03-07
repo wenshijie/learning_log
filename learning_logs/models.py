354cd3d89fd971784a6c2f07f691e7bf4711529d
unknown <276656491@qq.com>
2021-03-07 14:24:55 +0800

    添加条目完成

---
+++ learning_logs/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+# Create your models here.
+class Topic(models.Model):
+    """用户学习的主体"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+
+class Entry(models.Model):
+    """学到的某个主题的具体知识"""
+    # 当删除一条信息时（主题）与它级联的信息（条目）也会被删除 on_delete
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        if len(self.text) < 50:
+            return self.text
+        else:
+            return self.text[:50] + "..."
