50be63e4efc0667d0f604a7d8951866f746e01df
wenshijie <276656491@qq.com>
2021-03-10 19:37:58 +0800

    注册新用户

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
 """为应用程序users定义URL模式"""
 
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     # 登录页面
-    path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),  # 盲改可能有问题
+    path('register/', views.register, name='register')
 
 ]
