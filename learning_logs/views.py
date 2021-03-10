529d54e32c86ce2e39f22fc35972c0c63d38775c
wenshijie <276656491@qq.com>
2021-03-10 20:02:48 +0800

    19.3.1使用@login_required限制登录

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from . models import Topic,Entry
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
 def index(request):
     """学习笔记的主页"""
-    return render(request,'learning_logs/index.html')
+    return render(request, 'learning_logs/index.html')
 
+
+@login_required
 def topics(request):
     """显示所有主题"""
     topics = Topic.objects.order_by('date_added')
-    context = {'topics':topics}
-    return render(request,'learning_logs/topics.html',context)
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
-def topic(request,topic_id):
+
+@login_required
+def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic':topic , 'entries':entries}
-    return render(request,'learning_logs/topic.html',context)
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
 
+
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -35,11 +42,12 @@ def new_topic(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
-    context = {'form':form}
-    return render(request,'learning_logs/new_topic.html',context)
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
 
 
-def new_entry(request,topic_id):
+@login_required
+def new_entry(request, topic_id):
     """在特定的主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -52,13 +60,14 @@ def new_entry(request,topic_id):
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
 
-    context = {'topic':topic,'form':form}
-    return render(request,'learning_logs/new_entry.html',context)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
 
 
-def edit_entry(request,entry_id):
+@login_required
+def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
@@ -68,13 +77,9 @@ def edit_entry(request,entry_id):
         form = EntryForm(instance=entry)
     else:
         # POST提交的数据，对数据进行处理
-        form = EntryForm(instance=entry,data=request.POST)
+        form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic.id]))
-    context = {'entry':entry,'topic':topic,'form':form}
-    return render(request,'learning_logs/edit_entry.html',context)
-
-
-
-
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
